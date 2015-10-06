d8e08e716fb7ee5bad3da72d5ef79d4f0796aceb
Wilson <wilson.omokoro@andela.com>
2015-10-06 11:10:02 +0100

    Correct a bug in the delete implementation

--- index.php
+++ index.php
@@ -5,14 +5,14 @@ use Wilson\Crud;
 
 Crud::create(["slang"=>"cool", "description"=>"When something is awesome", "sample-sentence"=>"That's really cool."]);
 Crud::update(0, ["slang"=>"Hip", "description"=>"When something is trendy", "sample-sentence"=>"Hip style"]);
-
-$arr = Crud::read();
+//Crud::delete(1);
 
 echo '<table width="700" cellpadding="3" border="1" style="font-family:arial; font-size:12px;">';
 echo "<tr>";
 echo "<th><br /></th> <th>Word</th> <th>Meaning</th> <th>Example</th>";
 echo "</tr>";
 
+$arr = Crud::read();
 for($x=0; $x < count($arr); $x++) {
     echo "<tr valign='top'>";
     echo "<td>".($x+1).".</td>";
--- src/Crud.php
+++ src/Crud.php
@@ -20,12 +20,12 @@ class Crud
     public static function update($index, $newEntry)
     {
         //test that index is an integer and that newEntry is an array
-        UrbanWords::$data = array_splice(UrbanWords::$data, $index, 1);
+       array_splice(UrbanWords::$data, $index, 1, $newEntry);
     }
 
-    public static function delete()
+    public static function delete($index)
     {
-        //test that index is an integer and that newEntry is an array
-        UrbanWords::$data = array_splice(UrbanWords::$data, $index, 1);
+        //test that index is an integer
+        array_splice(UrbanWords::$data, $index, 1);
     }
 }
